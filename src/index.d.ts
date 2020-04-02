52828d0c9bb51919303a01bd3ff80916abaf4aa2
Pelle Johnsen <pjo@timemsystem.com>
2020-04-02 09:48:07 +0200

    Update typings, matching @types/jest-cucumber-fusion@0.6.0

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,17 +1,25 @@
-declare module "jest-cucumber-fusion" {
-    import { loadFeature } from "jest-cucumber";
-  
-    type CallBack = (...args: string[]) => void | Promise<void>;
-  
-    function Given(name: string | RegExp, callback: CallBack): void;
-    function When(name: string | RegExp, callback: CallBack): void;
-    function Then(name: string | RegExp, callback: CallBack): void;
-    function And(name: string | RegExp, callback: CallBack): void;
-    function But(name: string | RegExp, callback: CallBack): void;
-  
-    function Before(callback: () => void | Promise<void>): void;
-    function After(callback: () => void | Promise<void>): void;
-  
-    function Fusion(feature: string, options?: Parameters<typeof loadFeature>[1]): void;
-  }
-  
+// Type definitions for jest-cucumber-fusion 0.6
+// Project: https://github.com/b-yond-infinite-network/jest-cucumber-fusion#readme
+// Definitions by: Pelle Johnsen <https://github.com/pjoe>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Minimum TypeScript Version: 3.4
+
+import { loadFeature } from "jest-cucumber";
+
+export type CallBack = (
+  ...args: ReadonlyArray<string | Array<Record<string, string>>>
+) => void | Promise<void>;
+
+export function Given(name: string | RegExp, callback: CallBack): void;
+export function When(name: string | RegExp, callback: CallBack): void;
+export function Then(name: string | RegExp, callback: CallBack): void;
+export function And(name: string | RegExp, callback: CallBack): void;
+export function But(name: string | RegExp, callback: CallBack): void;
+
+export function Before(callback: () => void | Promise<void>): void;
+export function After(callback: () => void | Promise<void>): void;
+
+export function Fusion(
+  feature: string,
+  options?: Parameters<typeof loadFeature>[1]
+): void;
