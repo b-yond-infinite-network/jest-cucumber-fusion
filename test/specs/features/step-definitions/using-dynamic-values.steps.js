818498a05539307eaea99ce28571175bde81b927
Eugene <37618680+j8kin@users.noreply.github.com>
2021-08-22 08:18:14 +0300

    Test added which reveals the problem

--- test/specs/features/step-definitions/using-dynamic-values.steps.js
+++ test/specs/features/step-definitions/using-dynamic-values.steps.js
@@ -2,35 +2,55 @@ const { Before, Given, When, Then, Fusion } = require( '../../../../src' )
 
 const { BankAccount } = require( '../../../src/bank-account' )
 
-let myAccount
+let myAccounts
 		
-Before( () => { myAccount = new BankAccount() } )
+Before( () => { myAccounts = [] } )
 
-Given(/^my account balance is \$(\d+)$/, balance => {
-    myAccount.deposit(parseInt(balance))
+Given(/^I open new account with '(.+)' name$/, newAccountName => {
+    myAccounts.push(new BankAccount());
+    myAccounts[myAccounts.length - 1].name = newAccountName;
+})
+
+Given(/^my account#(\d+) balance is \$(\d+)$/, (nAcc, balance) => {
+    myAccounts[nAcc - 1].deposit(parseInt(balance))
 } )
 
-When(/^I get paid \$(\d+) for writing some awesome code$/, paycheck => {
-    myAccount.deposit(parseInt(paycheck))
+When(/^I get paid \$(\d+) for writing some awesome code from my account#(\d+)$/, (paycheck, nAcc) => {
+    myAccounts[nAcc - 1].deposit(parseInt(paycheck))
 } )
 
-Then(/^my account balance should be \$(\d+)$/, expectedBalance => {
-    expect(myAccount.balance).toBe(parseInt(expectedBalance))
+Then(/^my account#(\d+) balance should be \$(\d+)$/, (nAcc, expectedBalance) => {
+    expect(myAccounts[nAcc - 1].balance).toBe(parseInt(expectedBalance))
 } )
 
-Given(/^my account name is '(.*)'$/, nameAccount => {
-    myAccount.name = nameAccount
+Then(/^I have (\d+) accounts$/, nAccounts => {
+    expect(myAccounts.length).toBe(parseInt(nAccounts));
+})
+
+Given(/^my account#(\d+) name is '(.*)'$/, (nAcc, nameAccount) => {
+    myAccounts[nAcc - 1].name = nameAccount
 } )
 
-When(/^I get an new account name '(.*)' with a type (.*) from my old account named '(.*)'$/, ( nameNewAccount, typeAccount, nameOldAccount ) => {
+When(/^I get an new account#(\d+) name '(.*)' with a type (.*) from my old account named '(.*)'$/, ( nAcc, nameNewAccount, typeAccount, nameOldAccount ) => {
+    myAccounts[nAcc - 1].name = nameNewAccount
+    myAccounts[nAcc - 1].type = typeAccount
+} )
+
+Then(/^my account#(\d+) name should be "(.*)" and have a type (.*)$/, ( nAcc, nameAccount, typeAccount ) => {
+    expect(myAccounts[nAcc - 1].name).toBe( nameAccount )
+    expect(myAccounts[nAcc - 1].type).toBe( typeAccount )
+} )
+
+Then(/^my account#(\d+) should be:$/, (nAcc, table) => {
+    if (typeof table === 'string') return;
     
-    myAccount.name = nameNewAccount
-    myAccount.type = typeAccount
-} )
-
-Then(/^my account name should be "(.*)" and have a type (.*)$/, ( nameAccount, typeAccount ) => {
-    expect(myAccount.name).toBe( nameAccount )
-    expect(myAccount.type).toBe( typeAccount )
-} )
+    for (const [key, value] of Object.entries(myAccounts[nAcc - 1])) {
+        table.forEach(row => {
+            if (row.field === key) {
+                expect(value.toString()).toBe(row.value);
+            }
+        });
+    }
+})
 
 Fusion( '../using-dynamic-values.feature' )
